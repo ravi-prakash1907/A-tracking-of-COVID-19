b7fd7e365ac85042e098f535f8ce8d5f0b1a4ee4
Ravi Prakash <38535224+ravi-prakash1907@users.noreply.github.com>
2020-03-17 23:19:51 +0530

    Delete init.R

--- init.R
+++
@@ -1,6 +0,0 @@
-.libPaths( c( "~/anaconda3/envs/Data_Science_in_R/lib/R/library" , .libPaths() ) )
-
-# workspace
-load("~/Documents/A-tracking-of-2019-nCoV/.RData")
-
-source("~/Documents/A-tracking-of-2019-nCoV/COVID-19/dataCleaner.R")
