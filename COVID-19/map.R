5457d136d6015dbb6dc4888cae02cccf0b76b8d9
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2020-04-03 16:02:55 +0530

    Automatic update for new data

--- COVID-19/map.R
+++ COVID-19/map.R
@@ -199,8 +199,8 @@ visualize_on_map <- function(df, Path, index = 1) {
 }
 
 #####################################################################
-# index 54   --->   15th March
-newDay = 71
+# index 72   --->   2nd April
+newDay = 72
 visualize_on_map("ever.Affected", "PLOTS/maps/pngs/ever", index = newDay)    # pass index also to plot map(s) from index-th day
 visualize_on_map("highly.Affected", "PLOTS/maps/pngs/highly", index = newDay)  # by default index is 1
 
